50a3e35fcef1693c88d003f5722a16116eb13591
aravindhalahalli <aravind@byteally.com>
2023-09-25 02:14:25 +0530

    Basic route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Github, { githubInfoLoader } from './components/Github/Github'
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route path="/my-app/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
