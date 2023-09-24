90d38829a717d89156adfcec8e80d23c6dcb59c7
aravindhalahalli <aravind@byteally.com>
2023-09-25 01:36:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contacts/Contacts'
+import User from './components/User/User'
+import Github, { githubInfoLoader } from './components/Github/Github'
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="user/:userid" element={<User />} />
+        <Route loader={githubInfoLoader} path="github" element={<Github />} />
+      </Route>
+    )
+  );
+
+  return (
+    <>
+       <RouterProvider router={router} />
+    </>
+  )
+}
+
+export default App
